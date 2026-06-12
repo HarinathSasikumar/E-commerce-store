15b2195048d33ff1e6391fdc929082c8adff9542
HARINATH S <harinath0033@gmail.com>
2026-06-13 00:08:26 +0530

    Fix CORS issue

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ 
+  origin: function (origin, callback) {
+    callback(null, true);
+  }, 
+  credentials: true 
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
